7543feaa1143d4ee12f8df5dca0fc2a8bc84bc96
Matt Emson <m.emson@landguardsystems.com>
2021-12-03 14:19:49 +0000

    moved a lot of inline classes to their own files - as they should be

---
+++ VectorTileRenderer/Brush.cs
@@ -0,0 +1,12 @@
+﻿namespace VectorTileRenderer
+{
+    public class Brush
+    {
+        public int ZIndex { get; set; } = 0;
+        public Paint Paint { get; set; }
+        public string TextField { get; set; }
+        public string Text { get; set; }
+        public string GlyphsDirectory { get; set; } = null;
+        public Layer Layer { get; set; }
+    }
+}
---
+++ VectorTileRenderer/DoubleExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VectorTileRenderer
+{
+    static class DoubleExtension
+    {
+        private const double DefaultPrecision = 0.0001;
+
+        internal static bool BasicallyEqualTo(this double a, double b)
+        {
+            return a.BasicallyEqualTo(b, 0.0001);
+        }
+
+        internal static bool BasicallyEqualTo(this double a, double b, double precision)
+        {
+            return Math.Abs(a - b) <= precision;
+        }
+    }
+}
