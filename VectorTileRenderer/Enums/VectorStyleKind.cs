d80fcd247c5c87052f883c901001a2341eec6631
Matt Emson <m.emson@landguardsystems.com>
2021-12-08 11:31:41 +0000

    Added Positron style

--- VectorTileRenderer/Enums/VectorStyleKind.cs
+++ VectorTileRenderer/Enums/VectorStyleKind.cs
@@ -1,4 +1,4 @@
 ﻿namespace AliFlex.VectorTileRenderer.Enums
 {
-    public enum VectorStyleKind { Basic, Bright, Dark, Light, Liberty, Toner, Custom }
+    public enum VectorStyleKind { Basic, Bright, Dark, Light, Liberty, Toner, Positron, Custom }
 }
